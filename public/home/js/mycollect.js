6422bc9da17b6864cb4e54854edb75260109555b
Yuri Chen <pluto401zz@gmail.com>
2020-03-31 23:22:54 +0800

    更新收藏页

--- public/home/js/mycollect.js
+++ public/home/js/mycollect.js
@@ -1,12 +1,21 @@
 var data = getJson($('#data'));
-console.log(data);
-
 
 var list = $('.list');
 var listHtml = '';
 
 data.forEach(function(e) {
     if (e.cancel == 1) {
-        
+        listHtml += `
+        <li>
+            <a href="/home/goods/goodsdetail.html?id=${e.goods_id}&date_id=${e.rushdate_id}&time_id=${e.rushtime_id}">
+                <img src=${imageUrl + e.img}>
+                <div class="price">
+                    <span>￥${e.price}</span>
+                    <span>￥${e.orprice}</span>
+                </div>
+            </a>
+        </li>
+        `;
     }
-})
+});
+list.append(listHtml);
